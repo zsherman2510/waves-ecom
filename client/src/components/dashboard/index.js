6f915f834c7a313630ed078a1d67d7a7e4a6836d
zavion sherman <zsherman2510@hotmail.com>
2021-08-31 10:58:25 -0700

    updated dashboard

---
+++ client/src/components/dashboard/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayout from "hoc/dashboardLayout";
+
+const UserDashboard = () => {
+  return <DashboardLayout title="Overview">user dashboard</DashboardLayout>;
+};
+
+export default UserDashboard;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,18 +1,17 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import MainLayout from "./hoc/mainLayout";
+import Loader from "utils/loader";
 
-  
-import React, { useEffect, useState } from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import MainLayout from './hoc/mainLayout';
-import Loader from 'utils/loader';
+import { useDispatch, useSelector } from "react-redux";
+import { userIsAuth, userSignOut } from "store/actions/usersActions";
 
-import Header from './components/navigation/header';
-import Footer from './components/navigation/footer';
-import Home from './components/home';
-import RegisterLogin from './components/auth'
+import Header from "./components/navigation/header";
+import Footer from "./components/navigation/footer";
+import Home from "./components/home";
+import RegisterLogin from "./components/auth";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { userIsAuth, userSignOut } from 'store/actions/usersActions';
-
+import Dashboard from "./components/dashboard";
 
 const Routes = (props) => {
   const [loading, setLoading] = useState(true);
@@ -42,6 +41,7 @@ const Routes = (props) => {
           <Header users={users} signOutUser={signOutUser} />
           <MainLayout>
             <Switch>
+              <Route path="/dashboard" component={Dashboard} />
               <Route path="/sign_in" component={RegisterLogin} />
               <Route path="/" component={Home} />
             </Switch>
